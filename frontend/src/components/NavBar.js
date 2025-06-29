814c38c2b417eeed55d95af201d7c54e9b98f323
Jina Park <jinapark119@gmail.com>
2025-06-29 07:06:08 +0100

    Add page reload in Nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,29 +1,31 @@
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import './NavBar.css';
 import { GlobalContext } from '../contexts/GlobalContext';  // ThemeContext → GlobalContext로 변경
 
 function NavBar() {
   const { darkMode, toggleDarkMode } = useContext(GlobalContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = (path) => {
+    if (location.pathname === path) {
+      // 이미 그 페이지면 새로고침
+      window.location.reload();
+    } else {
+      // 다른 페이지면 이동
+      navigate(path);
+    }
+  };
 
   return (
     <nav className="navbar">
       <div className="nav-left">
-        <NavLink to="/" className="nav-link" end>
-          Intro
-        </NavLink>
-        <NavLink to="/home" className="nav-link" end>
-          Home
-        </NavLink>
-        <NavLink to="/apodarchive" className="nav-link">
-          APOD Archive
-        </NavLink>
-        <NavLink to="/favourites" className="nav-link">
-          Favourites
-        </NavLink>
-        <NavLink to="/history" className="nav-link">
-          History
-        </NavLink>
+        <NavLink to="/" className="nav-link" onClick={() => handleClick('/')}>Intro</NavLink>
+        <NavLink to="/home" className="nav-link" onClick={() => handleClick('/home')}>Home</NavLink>
+        <NavLink to="/apodarchive" className="nav-link" onClick={() => handleClick('/apodarchive')}>APOD Archive</NavLink>
+        <NavLink to="/favourites" className="nav-link" onClick={() => handleClick('/favourites')}>Favourites</NavLink>
+        <NavLink to="/history" className="nav-link" onClick={() => handleClick('/history')}>History</NavLink>
       </div>
       <div className="nav-right">
         <label className="switch">
